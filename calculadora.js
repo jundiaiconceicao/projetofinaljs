aa496f75162f67aa1219aa32145296e3662394fd
Jundiai Conceicao <primllh@gmail.com>
2023-12-07 12:53:40 -0300

    subindo projeto

---
+++ calculadora.js
@@ -0,0 +1,66 @@
+/* 
+1. Crie variáveis que capturem os valores que a usuária inserir e manipule-os nas funções 
+2. Exibir o resultado na tela
+*/
+const botaoSubtrair = document.getElementById("subtrair")
+const botaoMultiplicar = document.getElementById("multiplicar")
+
+function somar() {
+    const numero1 = parseFloat(document.querySelector("#numero1Soma").value)
+    const numero2 = parseFloat(document.querySelector("#numero2Soma").value)
+
+    const total = numero1 + numero2
+
+    document.querySelector(".resultado").setAttribute('id', 'resultadoSoma')
+    
+
+    const paragrafo = document.getElementById("resultadoSoma")
+    const resultado = document.createElement("strong")
+    paragrafo.appendChild(resultado)
+    resultado.innerText = total
+
+}
+
+
+function subtrair() {
+    const numero1 = parseFloat(document.querySelector("#numero1Sub").value)
+    const numero2 = parseFloat(document.querySelector("#numero2Sub").value)
+
+    const resultado = numero1 - numero2
+
+    document.getElementById("resultadoSub").innerHTML = resultado
+
+}
+
+
+
+function multiplicar() {
+    const numero1 = parseFloat(document.querySelector("#numero1Mult").value)
+    const numero2 = parseFloat(document.querySelector("#numero2Mult").value)
+
+    const resultado = numero1 * numero2
+
+    
+    document.getElementById("resultadoMult").innerHTML = resultado
+    
+}
+
+function dividir() {
+    const numero1 = parseFloat(document.querySelector("#numero1Div").value)
+    const numero2 = parseFloat(document.querySelector("#numero2Div").value)
+
+    const total = numero1 / numero2
+
+    document.querySelector("#resultadoDiv").setAttribute('id', 'resultadoDiv')
+    
+
+    const paragrafo = document.getElementById("resultadoDiv")
+    const resultado = document.createElement("strong")
+    paragrafo.appendChild(resultado)
+    resultado.innerText = total
+
+
+}
+
+botaoSubtrair.addEventListener('click', subtrair)
+botaoMultiplicar.addEventListener('click', multiplicar)
---
+++ media.js
@@ -0,0 +1,57 @@
+/* 
+1. Crie variáveis que capturem os valores que a usuária inserir e manipule-os nas funções 
+2. Exibir o resultado na tela
+*/
+
+const botaoSubtrair = document.getElementById("subtrair")
+const botaoMultiplicar = document.getElementById("multiplicar")
+
+function somar() {
+ const numero1= parseFloat(document.querySelector("#numero1Soma").value)
+ const numero2= parseFloat(document.querySelector("#numero2Soma").value)
+
+ const total = numero1 + numero2
+
+
+ const paragarafo = document.getElementById("resultadoSoma")
+ const resultado = document.createElement('strong')
+ paragarafo.appendChild(resultado)
+ resultado.innerText = total
+
+}
+
+
+
+function subtrair() {
+    const numero1= parseFloat(document.querySelector("#numero1Sub").value)
+    const numero2= parseFloat(document.querySelector("#numero2Sub").value)
+   
+    const resultado = numero1 - numero2
+
+    document.getElementById("resultadoSub").innerHTML = resultado
+ 
+}
+
+
+
+function multiplicar() {
+    const numero1= parseFloat(document.querySelector("#numero1Mult").value)
+    const numero2= parseFloat(document.querySelector("#numero2Mult").value)
+
+    const total = numero1 * numero2
+
+    const paragrafo = document.querySelector("#resultadoMult")
+    const resultado = document.createElement('strong')
+    paragrafo.appendChild(resultado)
+    resultado.innerText = total
+}
+
+function dividir() {
+
+}
+
+
+botaoSubtrair.addEventListener('click', subtrair)
+botaoMultiplicar.addEventListener('click', multiplicar)
+
+
